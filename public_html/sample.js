de525045fc27e0e618b774c270c7c6e7da0f0631
dudvik92 <66221191+dudvik92@users.noreply.github.com>
2021-02-25 20:54:56 +0300

    Update sample.js

--- public_html/sample.js
+++ public_html/sample.js
@@ -1,16 +1,3 @@
-<html>
-<head>
-<link rel="stylesheet" href="resource://content-accessible/plaintext.css">
-</head>
-<body>
-  <pre>
-function task(x) {
-  return x.toUpperCase();
-}
-  </pre>
-</body>
-</html>
-/*
 function task(x) {
   return x * (this.x**2);
-}*/
+}
