196b919316ae573fddd0bdd49a2375ef4599136c
dudvik92 <66221191+dudvik92@users.noreply.github.com>
2021-02-25 20:53:15 +0300

    Update and rename sample.html to sample.js

---
+++ public_html/sample.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+<link rel="stylesheet" href="resource://content-accessible/plaintext.css">
+</head>
+<body>
+  <pre>
+function task(x) {
+  return x.toUpperCase();
+}
+  </pre>
+</body>
+</html>
+/*
+function task(x) {
+  return x * (this.x**2);
+}*/
